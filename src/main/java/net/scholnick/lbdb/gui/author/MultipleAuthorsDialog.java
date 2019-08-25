3d299bd85e1e71e1faca389f76230677d6d91a8d
Steve Scholnick <scholnicks@gmail.com>
2019-08-25 12:36:34 -0400

    improved multi-select

--- src/main/java/net/scholnick/lbdb/gui/author/MultipleAuthorsDialog.java
+++ src/main/java/net/scholnick/lbdb/gui/author/MultipleAuthorsDialog.java
@@ -4,32 +4,44 @@ import net.scholnick.lbdb.domain.Author;
 import net.scholnick.lbdb.gui.BaseDialog;
 import net.scholnick.lbdb.gui.TrimmedTextField;
 import net.scholnick.lbdb.service.AuthorService;
+import net.scholnick.lbdb.util.LabelFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static javax.swing.JOptionPane.showConfirmDialog;
 import static net.scholnick.lbdb.util.GUIUtilities.center;
-import static net.scholnick.lbdb.util.GUIUtilities.showMessageDialog;
 
 @Component
 @Scope("prototype")
 public class MultipleAuthorsDialog extends BaseDialog {
-	private JTextField nameField;
-	private JTable resultsTable;
-	private AuthorService authorService;
-	
-	public MultipleAuthorsDialog() {
+	private JTextField    nameField;
+	private JTable        resultsTable;
+	private JTable        selectedTable;
+
+	private final AuthorService authorService;
+
+	private static final Dimension TABLE_DIMENSION = new Dimension(260,120);
+
+	@Autowired
+	public MultipleAuthorsDialog(AuthorService authorService) {
 		super();
+		this.authorService = authorService;
+
 		setTitle("Add Multiple Authors");
 		setModal(true);
 		setSize(300, 400);
 		center(this);
 		buildGUI();
-
 		getOKButton().setName("Done");
 	}
 
@@ -47,20 +59,55 @@ public class MultipleAuthorsDialog extends BaseDialog {
 	@Override
 	protected void buildGUI() {
 		getContentPane().setLayout(new BorderLayout());
-
 		getContentPane().add(getNameField(), BorderLayout.NORTH);
+		getContentPane().add(getCenterPanels(), BorderLayout.CENTER);
+		getContentPane().add(getButtonPanel(), BorderLayout.SOUTH);
+	}
 
+	private JPanel getCenterPanels() {
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.gridx = gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.insets = new Insets(5, 5, 0, 0);
+
+		gbc.insets = new Insets(5, 100, 0, 0);
+		panel.add(LabelFactory.createLabel("Results"), gbc);
+
+		gbc.gridy++;
+		gbc.insets = new Insets(5, 5, 0, 0);
 		JScrollPane pane = new JScrollPane(getResultsTable());
 		pane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		pane.setMinimumSize(TABLE_DIMENSION);
+		panel.add(pane, gbc);
 
-		getContentPane().add(pane, BorderLayout.CENTER);
-		getContentPane().add(getButtonPanel(), BorderLayout.SOUTH);
+		gbc.gridy++;
+		gbc.insets = new Insets(5, 100, 0, 0);
+		panel.add(LabelFactory.createLabel("Selected"), gbc);
+
+		gbc.gridy++;
+		gbc.insets = new Insets(5, 5, 0, 0);
+		JScrollPane selectedPane = new JScrollPane(getSelectedTable());
+		selectedPane.setMinimumSize(TABLE_DIMENSION);
+		selectedPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel.add(selectedPane, gbc);
+
+		return panel;
 	}
 
 	private JTextField getNameField() {
 		if (nameField == null) {
 			nameField = new TrimmedTextField(20,100);
-			nameField.addActionListener(e -> search());
+			nameField.addActionListener(e -> search(false));
+			nameField.addKeyListener(new KeyAdapter() {
+				@Override public void keyReleased(KeyEvent e) {
+					if (nameField.getText() != null && nameField.getText().length() > 3) {
+						search(true);
+					}
+				}
+			});
 		}
 		return nameField;
 	}
@@ -75,56 +122,67 @@ public class MultipleAuthorsDialog extends BaseDialog {
 			resultsTable = new JTable(new AuthorTableModel());
 			resultsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			resultsTable.setCellSelectionEnabled(false);
-			resultsTable.setRowSelectionAllowed(false);
+			resultsTable.setRowSelectionAllowed(true);
 			resultsTable.getTableHeader().setReorderingAllowed(false);
 			resultsTable.getTableHeader().setUI(null);
+
+			resultsTable.addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent event) {
+					if (event.getClickCount() == 2) {
+						AuthorTableModel resultesModel = (AuthorTableModel) getResultsTable().getModel();
+
+						AuthorTableModel selectedModel = (AuthorTableModel) getSelectedTable().getModel();
+						selectedModel.add(resultesModel.get(resultsTable.getSelectedRow()));
+						clear();
+						getNameField().requestFocus();
+						repaintScreen();
+					}
+				}
+			});
 		}
 		return resultsTable;
 	}
 
-	private void search() {
+	private JTable getSelectedTable() {
+		if (selectedTable == null) {
+			selectedTable = new JTable(new AuthorTableModel());
+			selectedTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			selectedTable.setCellSelectionEnabled(false);
+			selectedTable.setRowSelectionAllowed(false);
+			selectedTable.getTableHeader().setReorderingAllowed(false);
+			selectedTable.getTableHeader().setUI(null);
+		}
+		return selectedTable;
+	}
+
+	private void search(boolean existingOnly) {
 		AuthorTableModel model = (AuthorTableModel) getResultsTable().getModel();
+		model.clear();
 
-		java.util.List<Author> foundAuthors = authorService.search(getNameField().getText());
+		List<Author> foundAuthors = authorService.search(getNameField().getText());
 
-		if (foundAuthors.isEmpty()) {
+		if (! foundAuthors.isEmpty()) {
+			foundAuthors.forEach(model::add);
+			repaintScreen();
+		}
+		else {
+			if (existingOnly) {
+				repaintScreen();
+				return;
+			}
+
 			String fullName = getNameField().getText();
 
 			String message = "Author ( " + fullName + " ) not found.  Add?";
 			int choice = showConfirmDialog(this, message, "Add Author?", JOptionPane.YES_NO_OPTION);
 			if (choice == JOptionPane.YES_OPTION) {
 				model.add(Author.of(fullName));
-				getNameField().setText("");
-				getNameField().requestFocus();
 			}
 		}
-		else {
-			if (foundAuthors.size() > 1) {
-				showMessageDialog("Multiple Matches for search criteria. Narrow your search");
-			}
-			else {
-				Author a = foundAuthors.get(0);
-				if (model.getAllAuthors().contains(a)) {
-					showMessageDialog(a.getName() + " has already been added. Skipping.");
-				}
-				else {
-					model.add(foundAuthors.get(0));
-				}
-
-				getNameField().setText("");
-				getNameField().requestFocus();
-			}
-		}
-
-		repaintScreen();
 	}
 
 	public java.util.List<Author> getAuthors() {
-		return ((AuthorTableModel) getResultsTable().getModel()).getAllAuthors();
-	}
-
-	@Autowired
-	public void setAuthorService(AuthorService authorService) {
-		this.authorService = authorService;
+		java.util.List<Author> data = ((AuthorTableModel) getSelectedTable().getModel()).getAllAuthors();
+		return data.stream().distinct().sorted().collect(toList());
 	}
 }
