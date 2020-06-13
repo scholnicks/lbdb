d332eb72746eb33dac1a0852ef612842c18aa51f
Steve Scholnick <scholnicks@gmail.com>
2020-06-13 12:53:38 -0400

    updated dependencies

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -4,5 +4,5 @@ cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
      -Dcom.apple.mrj.application.live-resize=true \
      -Dlbdb.database.type=production \
--jar laurel-book-database-0.1.0.jar 1>/dev/null 2>&1
+-jar laurel-book-database-4.6.1.jar 1>/dev/null 2>&1
 
