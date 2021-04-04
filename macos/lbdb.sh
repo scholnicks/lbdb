a1d356d593c41e4f9cda0520f979599ba3730f6a
Steve Scholnick <scholnicks@gmail.com>
2021-04-04 09:14:48 -0400

    back to homebrew's Java installation

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/Users/steve/.sdkman/candidates/java/16.0.0.hs-adpt
-
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
 cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
      -Dcom.apple.mrj.application.live-resize=true \
