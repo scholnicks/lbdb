9f25a16cc0cc2adc2244c23f6a7d50ed95b3cfe7
Steve Scholnick <scholnicks@gmail.com>
2021-06-01 08:55:16 -0400

    fixed package. Deprecated deploy.sh

--- package.sh
+++ package.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+appName="Laurel Book Database"
+version="4.6.1"
+vendor="Steve Scholnick"
+
+rm -f "${appName}-${version}".dmg
+
 ./gradlew clean build || exit -1
 
-jpackage --input build/ \
-  --name "Laurel-Book-Database" \
-  --main-jar /Users/steve/development/java/lbdb/build/libs/laurel-book-database-4.6.1.jar \
-  --icon "/Users/steve/development/java/lbdb/macos/book_case.icns" \
-  --app-version "4.6.1" \
-  --vendor "Steve Scholnick" \
-  --copyright "Copyright 2021 Steve Scholnick" \
-  --mac-package-name "Laurel Book Database" \
+jpackage \
+  --input            build \
+  --name             "${appName}" \
+  --main-jar         "$HOME/development/java/lbdb/build/libs/laurel-book-database-${version}.jar" \
+  --icon             "$HOME/development/java/lbdb/macos/book_case.icns" \
+  --java-options     "-Dlbdb.environment=production" \
+  --app-version      "${version}" \
+  --vendor           "${vendor}" \
+  --copyright        "Copyright 2021 ${vendor}" \
+  --mac-package-name "${appName}" \
   --verbose
+
+open "${appName}-${version}".dmg
+
+exit 0
