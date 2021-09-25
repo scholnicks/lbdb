0dddba4bc7d9212812954128e88f22c2a800412e
Steve Scholnick <scholnicks@gmail.com>
2021-09-25 09:41:49 -0400

    upgraded to Java 17

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
 cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
      -Dcom.apple.mrj.application.live-resize=true \
