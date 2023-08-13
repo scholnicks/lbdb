9a6bfc7f4521b3e5c0d639060a1aefcad535e3fd
Steve Scholnick <scholnicks@gmail.com>
2023-08-13 10:09:41 -0400

    updated version number

--- src/main/java/net/scholnick/lbdb/BooksDB.java
+++ src/main/java/net/scholnick/lbdb/BooksDB.java
@@ -30,7 +30,7 @@ public final class BooksDB extends JFrame {
     private ExportService exportService;
 
     public static final Dimension WINDOW_SIZE = new Dimension(1000, 850);
-    private static final String       VERSION = "Version 7.0.1";
+    private static final String       VERSION = "Version 7.1.0";
 
     public static final Color BACKGROUND_COLOR = new Color(4,106,56);
     public static final Color FOREGROUND_COLOR = Color.white;
