c07d6f7c992a5b546756a947fdf8204d0b90c416
Steve Scholnick <scholnicks@gmail.com>
2024-10-06 09:08:04 -0400

    upgraded Java and Spring

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vi: set syntax=sh ts=4 sw=4 sts=4 et ff=unix ai si :
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-23.jdk/Contents/Home
 cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
      -Dcom.apple.mrj.application.live-resize=true \
