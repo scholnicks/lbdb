fe34c0354fecc0ba44d819badbcfad08194a1724
Steve Scholnick <scholnicks@gmail.com>
2025-03-15 14:22:19 -0400

    made title export case-insensitive

--- src/main/java/net/scholnick/lbdb/repository/BookRepository.java
+++ src/main/java/net/scholnick/lbdb/repository/BookRepository.java
@@ -168,21 +168,19 @@ public class BookRepository {
     }
 
     private static final String TITLES_EXPORT_SQL = """
-            select
-               b.book_title as "Title",\s
-               m.med_desc as "Media",
-               group_concat(a.auth_name) as "Authors"
-            from
-               Book b
-            join
-               Media_Type m on m.med_id=b.med_id
-            join
-               Author_Book_Xref x on x.book_id=b.book_id
-            join
-               Author a on a.auth_id=x.auth_id
-            group by
-               b.book_title, m.med_desc
-            order by
-               1"""
-        ;
+        select
+           b.book_title as "Title", m.med_desc as "Media", group_concat(a.auth_name) as "Authors"
+        from
+           Book b
+        join
+           Media_Type m on m.med_id=b.med_id
+        join
+           Author_Book_Xref x on x.book_id=b.book_id
+        join
+           Author a on a.auth_id=x.auth_id
+        group by
+           b.book_title, m.med_desc
+        order by
+           lower(b.book_title)
+       """;
 }
