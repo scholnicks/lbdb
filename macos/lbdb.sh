d9267c6abe56e0443d8f282f611e29b1bda9e5e5
Steve Scholnick <scholnicks@gmail.com>
2025-05-04 14:15:51 -0400

    Switch t zgc

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -4,6 +4,7 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-24.jdk/Contents/Home
 cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
+     -XX:+UseZGC \
      -Dcom.apple.mrj.application.live-resize=true \
      --enable-native-access=ALL-UNNAMED \
      -Dlbdb.environment=production \
