2fa45c5f6aaa03537287223abec9a37b242a8c95
Steve Scholnick <scholnicks@gmail.com>
2025-10-04 08:55:57 -0400

    upgraded Java and dependencies

--- macos/lbdb.sh
+++ macos/lbdb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vi: set syntax=sh ts=4 sw=4 sts=4 et ff=unix ai si :
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-24.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-25.jdk/Contents/Home
 cd `dirname $0`/../Resources
 java -Dapple.laf.useScreenMenuBar=true -Xdock:name="Laurel Book Database" -Xdock:icon=book_case.icns \
      -XX:+UseZGC \
