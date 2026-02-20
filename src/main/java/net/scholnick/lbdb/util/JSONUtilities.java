5a42d23c755f643c855a2a1ccfaf63b156139acc
Steve Scholnick <scholnicks@gmail.com>
2026-02-20 09:29:11 -0500

    updated dependencies

--- src/main/java/net/scholnick/lbdb/util/JSONUtilities.java
+++ src/main/java/net/scholnick/lbdb/util/JSONUtilities.java
@@ -3,9 +3,15 @@ package net.scholnick.lbdb.util;
 import tools.jackson.core.JacksonException;
 import tools.jackson.databind.ObjectMapper;
 
+/**
+ * JSONUtilities is a collection of utility methods for working with JSON.
+ *
+ * @author Steve Scholnick <scholnicks@gmail.com>
+ */
 public final class JSONUtilities {
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
+    /** Converts the given object to a JSON string. */
     public static <T> T fromJSON(String json, Class<T> clazz) {
         try {
             return MAPPER.readValue(json, clazz);
